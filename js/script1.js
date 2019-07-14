0e59b3ec05ff0d5f0a853e9ea94eb8a5f8cdb416
Bchizi <leonbichanga@gmail.com>
2019-07-15 01:07:16 +0300

    enter functions

---
+++ js/script1.js
@@ -0,0 +1,18 @@
+var cc, yy, mm, dd;
+function input() {
+    cc = parseInt(document.getElementById("century").value);
+    yy = parseInt(document.getElementById("year").value);
+    mm = parseInt(document.getElementById("month").value);
+    dd = parseInt(document.getElementById("day").value);
+}
+// var gender = (document.getElementsById('your gender').value);
+
+function day() {
+    input();
+    answer = (((cc / 4) - 2 * cc - 1) + ((5 * yy / 4)) + ((26 * (mm + 1) / 10)) + dd) % 7;
+    answer -= 1;
+    console.log(answer);
+    return (Math.floor(answer));
+
+}
+
